77604ec3edf7e3ecb56b746f5db1e2077874ecd2
aldothecoder <aldodiazfitness@gmail.com>
2024-07-16 00:57:34 -0500

    Add files via upload

---
+++ TaskManagement/src/main/java/com/example/TaskManagement/Model/User.java
@@ -0,0 +1,32 @@
+package com.example.TaskManagement.Model;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "USER")
+public class User{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private int id;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+    @Column(name = "PASSWORD")
+    private String password;
+    
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+}
---
+++ TaskManagement/src/main/java/com/example/TaskManagement/TaskManagementController/TaskManagementController.java
@@ -0,0 +1,5 @@
+package com.example.TaskManagement.TaskManagementController;
+
+public class TaskManagementController{
+
+}
